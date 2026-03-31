49097287cf4e62d99c5775f30b8ef9776513ed22
Daniel Torquato <daniel@torquato.xyz>
2026-04-01 00:02:16 +0100

    MyApps-9 POC: Add basic setup for Instrumented tests.

--- app/src/androidTest/java/xyz/torquato/myapps/web/WebInstrumentedTest.kt
+++ app/src/androidTest/java/xyz/torquato/myapps/web/WebInstrumentedTest.kt
@@ -2,8 +2,6 @@ package xyz.torquato.myapps.web
 
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
@@ -11,10 +9,13 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.Until
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import xyz.torquato.myapps.MenuActivity
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -28,9 +29,12 @@ class WebInstrumentedTest {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
-    @get:Rule
-    val activityRule = ActivityScenarioRule(MenuActivity::class.java)
+    //@get:Rule
+    //val activityRule = ActivityScenarioRule(MenuActivity::class.java)
 
+    private val device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+
+    /*
     @Test
     fun when_open_web_query_given_open_web_search_then_query_bar_is_visible() {
         // Given
@@ -43,6 +47,20 @@ class WebInstrumentedTest {
 
         // Then
         composeTestRule.onNodeWithTag("QUERY_BAR").assertIsDisplayed()
+    }
+     */
+
+    @Test
+    fun when_given_then_2() {
+        // Given
+        /* open using activity rule */
+        device.pressHome()
+
+        val appUT = device.findObject(By.text("Clock"))
+
+        appUT.clickAndWait(Until.newWindow(), 3000)
+
+        device.wait(Until.hasObject(By.text("alarms")), 1_000L)
     }
 
     private fun AndroidComposeTestRule<ActivityScenarioRule<ComponentActivity>, ComponentActivity>.clickButtonWithTag(
